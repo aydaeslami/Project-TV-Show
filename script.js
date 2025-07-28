67cd53d3d96ffb6a2096b6a5f43449a7e8b8f65b
Aida Eslami <ayda.eslami@gmail.com>
2025-07-28 23:10:07 +0100

    Add API fetch with search

--- script.js
+++ script.js
@@ -1,28 +1,47 @@
-let allEpisodes =[]
+let allEpisodes = [];
 
-function setup() {
-  allEpisodes = getAllEpisodes();
+async function setup() {
+  // allEpisodes = getAllEpisodes();
 
-  renderFilm(allEpisodes);
-  populateSelectMenu(allEpisodes);
+  // API
+  const messageElement = document.getElementById("message"); // var for Loading msg
+  try {
+    messageElement.textContent = "Loading episodes..."; // msg Loading
+    const response = await fetch("https://api.tvmaze.com/shows/82/episodes");
+    // console.log(response);
+    if (!response.ok) throw new Error("Failed to load data");
+    allEpisodes = await response.json();
+    // console.log(allEpisodes);
+    messageElement.textContent = "";
 
-  document.getElementById("search-input").addEventListener("keyup", onSearch)
-  document.getElementById("episode-select").addEventListener("change" , onSelect)
+    renderFilm(allEpisodes);
+    populateSelectMenu(allEpisodes);
+  } catch (error) {
+    messageElement.textContent =
+      "Error loading episodes. Please try again later.";
+  } // END OF API
+
+  // Event listener
+  document.getElementById("search-input").addEventListener("keyup", onSearch);
+  document
+    .getElementById("episode-select")
+    .addEventListener("change", onSelect);
 }
 
-
 // search function
-function onSearch (event){
+function onSearch(event) {
   const searchTerm = event.target.value.toLowerCase();
-  const filtered = allEpisodes.filter( (ep) => {
+  const filtered = allEpisodes.filter((ep) => {
     return (
       ep.name.toLowerCase().includes(searchTerm) ||
       ep.summary.toLowerCase().includes(searchTerm)
-    )
-  })
-  const count = document.getElementById("match-count")
-  count.innerText = `Display ${filtered.length}/77 episodes.`
-  renderFilm(filtered)
+    );
+  });
+  const count = document.getElementById("match-count");
+
+  count.innerText = `Display ${filtered.length}/${allEpisodes.length} episodes.`;
+
+  renderFilm(filtered);
 }
 
 function onSelect(event) {
@@ -41,18 +60,14 @@ function onSelect(event) {
 function padNumber(num) {
   return num.toString().padStart(2, "0");
 }
-// creates S01E12
+
 function formatEpisodeCode(season, number) {
   return `S${padNumber(season)}E${padNumber(number)}`;
 }
 
-
 function populateSelectMenu(episodes) {
   const select = document.getElementById("episode-select");
-
-  // Clear existing options except "-- Show All --"
   select.innerHTML = '<option value="all">-- Show All Episodes --</option>';
-
   episodes.forEach((ep, index) => {
     const code = formatEpisodeCode(ep.season, ep.number);
     const option = document.createElement("option");
@@ -62,12 +77,9 @@ function populateSelectMenu(episodes) {
   });
 }
 
-
-// render template function
 function renderFilm(episodeList) {
   const containerEpisode = document.getElementById("root");
-  containerEpisode.innerHTML = ""
-
+  containerEpisode.innerHTML = "";
   const templateEpisode = document.getElementById("episode-template");
 
   episodeList.forEach((episode) => {
@@ -77,7 +89,6 @@ function renderFilm(episodeList) {
       episode.name + " - " + formatEpisodeCode(episode.season, episode.number);
 
     clone.querySelector(".summary").innerHTML = episode.summary;
-
     containerEpisode.append(clone);
   });
 }
